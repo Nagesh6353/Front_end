7a41b1b11124d227d20f7e8b3c3b404ca73fedc0
Nagesh6353 <kumavatnagesh15@gmail.com>
2025-12-16 20:18:45 +0530

    JS Assignment

---
+++ React/demo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassComponents from "./components/ClassComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello This is Function</h1>
+      <ClassComponents />
+      <ClassComponents />
+    </div>
+  )
+}
+
+export default App;
